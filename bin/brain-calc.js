da9bfb10d8ea1ca4b378edf57c93cede04db775a
marv <manchkhava.bozheva@gmail.com>
2025-03-12 17:30:56 +0300

    update paths

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from './games/brain-calc.js';
+import startGame from '../games/brain-calc.js';
 
 startGame();
--- bin/brain.even.js
+++ bin/brain.even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from './games/brain-even.js';
+import startGame from '../games/brain-even.js';
 
 startGame();
